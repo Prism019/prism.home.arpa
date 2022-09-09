545fbff98e58e8dbe62d10f3f9e3eb0c20b8a6ae
Prism Daymon <prismdaymon@gmail.com>
2022-09-09 00:02:56 -0700

    add opencl support

--- hosts/ristaccia.nix
+++ hosts/ristaccia.nix
@@ -54,7 +54,13 @@
   };
 
   hardware = {
-    opengl.driSupport32Bit = true;
+    opengl = {
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        rocm-opencl-icd
+        rocm-opencl-runtime
+      ];
+    };
     pulseaudio.enable = false;
   };
 
